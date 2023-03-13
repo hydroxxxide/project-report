1df5d170e424e62e92b31a609581836e64096914
Khashem <hashemehraz@gmail.com>
2023-03-13 19:33:28 +0600

    first commit

---
+++ src/main/java/com/example/projectreport/ProjectReportApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectReportApplication.class, args);
+	}
+
+}
