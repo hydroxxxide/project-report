c709c0119ba8dc7bd515b85ba268410166e5a9b5
Khashem <hashemehraz@gmail.com>
2023-03-15 00:24:28 +0600

    report control and service update

--- src/main/java/com/example/projectreport/controller/ReportController.java
+++ src/main/java/com/example/projectreport/controller/ReportController.java
@@ -1,10 +1,10 @@
 package com.example.projectreport.controller;
 
 import com.example.projectreport.entity.Report;
+import com.example.projectreport.enums.ReportStatus;
 import com.example.projectreport.service.ReportService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,7 +13,28 @@ import java.util.List;
 @RequestMapping("/report")
 public class ReportController {
     ReportService reportService;
-    public List<Report> getAllReports(){
-        return reportService
+    @PostMapping("/create")
+    Report createReport(@RequestBody Report report){
+        return reportService.createReport(report);
+    }
+    @GetMapping("/all")
+    List<Report> getAllReports(){
+        return reportService.getAllReports();
+    }
+    @GetMapping("/{id}")
+    Report getReportById(@PathVariable Long id){
+        return reportService.getReportById(id);
+    }
+    @PostMapping("/update/{id}")
+    Report updateReport(@PathVariable Long id, @RequestBody Report report){
+        return reportService.updateReport(id, report);
+    }
+    @PostMapping("/delete/{id}")
+    void deleteReport(@PathVariable Long id){
+        reportService.deleteReport(id);
+    }
+    @PostMapping("/change-status/{id}")
+    Report changeStatus(@PathVariable Long id, @RequestParam ReportStatus reportStatus){
+        return reportService.changeReportStatus(id, reportStatus);
     }
 }
--- src/main/java/com/example/projectreport/service/ReportService.java
+++ src/main/java/com/example/projectreport/service/ReportService.java
@@ -1,6 +1,7 @@
 package com.example.projectreport.service;
 
 import com.example.projectreport.entity.Report;
+import com.example.projectreport.enums.ReportStatus;
 import com.example.projectreport.repository.ReportRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,10 +20,24 @@ public class ReportService {
         return reportRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Report with id " + id + "not found"));
     }
-    public Report createReport(Long id){
-        Report report = getReportById(id);
+    public Report createReport(Report report){
+        report.setReportStatus(ReportStatus.NEW);
         return reportRepository.save(report);
     }
-    public
+    public Report updateReport(Long id, Report report){
+        Report newReport = getReportById(id);
+        newReport.setText(report.getText());
+        newReport.setTitle(report.getTitle());
+        newReport.setTasks(report.getTasks());
+        return reportRepository.save(newReport);
+    }
+    public void deleteReport(Long id){
+        reportRepository.deleteById(id);
+    }
+    public Report changeReportStatus(Long id, ReportStatus reportStatus){
+        Report report = getReportById(id);
+        report.setReportStatus(reportStatus);
+        return reportRepository.save(report);
+    }
 
 }
